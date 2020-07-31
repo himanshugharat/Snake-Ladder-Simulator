3d14254517ef11538ef127d98f3a4cefd25dd247
himanshugharat <himanshugharat44@gmail.com>
2020-07-31 08:13:19 -0500

    ADDED:now the game continues till 100th position

--- game.sh
+++ game.sh
@@ -2,16 +2,19 @@
 #constants
 START_POSITION=0
 position=$START_POSITION
-dieRoll=$((RANDOM%6 +1))
-option=$((RANDOM%3))
-case $option in
-	0)position=$((position));;
-	1)position=$((position+dieRoll));;
-	2)result=$((position-dieRoll))
-		if [ $result -gt 0 ]
-		then
-			position=$((position-dieRoll))
-		else
-			position=$position
-		fi;;
-esac
+while [ $position -le 100 ]
+do
+	dieRoll=$((RANDOM%6 +1))
+	option=$((RANDOM%3))
+	case $option in
+		0)position=$((position));;
+		1)position=$((position+dieRoll));;
+		2)result=$((position-dieRoll))
+			if [ $result -gt 0 ]
+			then
+				position=$((position-dieRoll))
+			else
+				position=$position
+			fi;;
+	esac
+done
