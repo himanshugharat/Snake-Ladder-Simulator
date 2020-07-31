84af6668535984e91214e79c38d5f645f4723988
himanshugharat <himanshugharat44@gmail.com>
2020-07-31 08:07:13 -0500

    ADDED:we have theladder snake fuctionality

--- game.sh
+++ game.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -x
 #constants
 START_POSITION=0
+position=$START_POSITION
 dieRoll=$((RANDOM%6 +1))
+option=$((RANDOM%3))
+case $option in
+	0)position=$((position));;
+	1)position=$((position+dieRoll));;
+	2)result=$((position-dieRoll))
+		if [ $result -gt 0 ]
+		then
+			position=$((position-dieRoll))
+		else
+			position=$position
+		fi;;
+esac
