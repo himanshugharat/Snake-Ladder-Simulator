1197989e9856db02cfc9329dbe504db4500cc8f5
himanshugharat <himanshugharat44@gmail.com>
2020-08-01 00:40:01 -0500

    ADDED:now the game stops at exact 100th place win

--- game.sh
+++ game.sh
@@ -3,19 +3,31 @@
 START_POSITION=0
 position=$START_POSITION
 #complete game till 100!!!
-while [ $position -le 100 ]
-do
-	dieRoll=$((RANDOM%6 +1))
-	option=$((RANDOM%3))
-	case $option in
-		0)position=$((position));;
-		1)position=$((position+dieRoll));;
-		2)result=$((position-dieRoll))
-			if [ $result -gt 0 ]
-			then
-				position=$((position-dieRoll))
-			else
-				position=$position
-			fi;;
-	esac
-done
+function playGame {
+ 	while [ $position -lt 100 ]
+	do
+		dieRoll=$((RANDOM%6 +1))
+		option=$((RANDOM%3))
+		case $option in
+			0)position=$((position));;
+
+			1)placeResult=$((position+dieRoll))
+				if [ $placeResult -le 100 ]
+				then
+					position=$((position+dieRoll))
+				else
+					position=$position
+				fi;;
+
+			2)placeResult=$((position-dieRoll))
+				if [ $placeResult -gt 0 ]
+				then
+					position=$((position-dieRoll))
+				else
+					position=$position
+				fi;;
+		esac
+	done
+}
+
+playGame
