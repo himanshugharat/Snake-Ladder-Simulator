8f24f94224cc21d8bc2fcedf3e1ec849462b7b5a
himanshugharat <himanshugharat44@gmail.com>
2020-08-03 04:35:28 -0500

    REFACTOR: now the code is genric no repitation of code

--- game.sh
+++ game.sh
@@ -3,103 +3,68 @@
 #constants
 START_POSITION=0
 positionOfPlayer1=$START_POSITION
+WIN_PLACE=100
+
+#vaiables
 dieCountOfPlayer1=0
 dieCountofPlayer2=0
 positionOfPlayer2=$START_POSITION
-
-# the die roll
-function dieRolled {
-	if [ $1 -eq 2 ]
-	then
-		dieRoll=$((RANDOM%6 +1))
-		((dieCountOfPlayer2++))
-	else
-		diceRoll=$((RANDOM%6 +1))
-		((dieCountOfPlayer1++))
-	fi
-}
+position=0
 
 #the choice for ladder same position or snake bite
 function playOptions {
-	option=$((RANDOM%3))
-	case $option in
-		0)if [ $1 -eq 1 ]
+dieRoll=$1
+option=$2
+position=$3
+case $option in
+	0)
+		position=$((position));;
+	1)
+		placeResult=$((position+dieRoll))
+		if [ $placeResult -le $WIN_PLACE ]
 		then
-			positionOfPlayer1=$((positionOfPlayer1))
-			echo "the players$1 position is $positionOfPlayer1"
+			position=$((position+dieRoll))
+			position="$(playOptions $(( RANDOM % 6 + 1 )) $(( RANDOM % 3 )) $position)"
 		else
-			positionOfPlayer2=$((positionOfPlayer2))
-			echo "the players$1 position is $positionOfPlayer2"
+			position=$position
 		fi;;
-		1)	if [ $1 -eq 1 ]
-			then
-				placeResult=$((positionOfPlayer1+dieRoll))
-				if [ $placeResult -le 100 ]
-				then
-					positionOfPlayer1=$((positionOfPlayer1+dieRoll))
-					echo "the players$1 position moved by $dieRoll is $positionOfPlayer1 after the ladder"
-					dieRolled $1
-					playOptions $1
-				else
-					positionOfPlayer1=$positionOfPlayer1
-					echo "the players position$1 is $positionOfPlayer1"
-				fi
-			else
-				placeResult=$((positionOfPlayer2+dieRoll))
-				if [ $placeResult -le 100 ]
-				then
-					positionOfPlayer2=$((positionOfPlayer2+dieRoll))
-					echo "the players$2 position moved by $dieRoll is $positionOfPlayer2 after the ladder"
-				else
-					positionOfPlayer2=$positionOfPlayer2
-					echo "the players$2 position is $positionOfPlayer2"
-				fi
-			fi;;
-
-		2)	if [ $1 -eq 1 ]
-			then
-				placeResult=$((positionOfPlayer1-dieRoll))
-				if [ $placeResult -gt 0 ]
-				then
-					positionOfPlayer1=$((positionOfPlayer1-dieRoll))
-					echo "the players$1 position moved by $dieRoll is $positionOfPlayer1 after snake bite "
-				else
-					positionOfPlayer1=0
-					echo "the players$1 position is $positionOfPlayer1"
-				fi
-			else
-				placeResult=$((positionOfPlayer2-dieRoll))
-				if [ $placeResult -gt 0 ]
-				then
-					positionOfPlayer2=$((positionOfPlayer2-dieRoll))
-					echo "the players$1 position moved by $dieRoll is $positionOfPlayer2 after snake bite "
-				else
-					positionOfPlayer2=0
-					echo "the players$1 position is $positionOfPlayer2"
-				fi
-			fi;;
-	esac
+	2)	placeResult=$((position-dieRoll))
+		if [ $placeResult -gt $START_POSITION ]
+		then
+			position=$((position-dieRoll))
+		else
+			position=0
+		fi;;
+esac
+echo $position
 }
 
 #the running function for exact 100 th place
 function playGame {
- 	while [ $positionOfPlayer1 -lt 100 ] && [ $positionOfPlayer2 -lt 100 ]
-	do
-		dieRolled 1
-		playOptions 1
-		dieRolled 2
-		playOptions 2
-	done
+while :
+do
+	if [ $positionOfPlayer1 -eq $WIN_PLACE ] || [ $positionOfPlayer2 -eq $WIN_PLACE ]
+	then
+		break
+	fi
+	((dieCountOfPlayer1++))
+	positionOfPlayer1="$(playOptions $((RANDOM % 6+1)) $((RANDOM %3 )) $positionOfPlayer1)"
+	echo "position of player 1 is $positionOfPlayer1"
+	echo "positin of player 2 is $positionOfPlayer2"
+	((dieCountOfPlayer2++))
+	positionOfPlayer2="$(playOptions $((RANDOM % 6+1)) $((RANDOM %3 )) $positionOfPlayer2)"
+done
 }
 
 
 #main code
 playGame
 
-#condition to check the winner
-if [ $positionOfPlayer1 -gt $positionOfPlayer2 ]
+if [ $positionOfPlayer1 -eq $WIN_PLACE ]
 then
-	echo "player1 wins the game and has rolled dice for $dieCountOfPlayer1"
+	echo "player 1 won"
+	echo "Dice rolled were $dieCountOfPlayer1"
 else
-	echo "player2 wins the game and has eolled dice for $dieCountOfPlayer2"
+	echo "player 2 won"
+	echo "Dice rolled were $dieCountOfPlayer2"
 fi
